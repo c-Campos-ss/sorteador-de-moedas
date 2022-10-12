b8eda1c2a9fb61443798f3e3e625b0d846070925
Cauã Campos <73807067+c-Campos-ss@users.noreply.github.com>
2022-10-12 00:05:50 -0300

    Add files via upload

---
+++ SorteadorDeMoeda/SorteadorDeMoeda/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SorteadorDeMoeda
+{
+    public partial class MainWindow : Window
+    {
+        Random rnd = new Random();
+        int chance;
+        double porcentagemCara;
+        double porcentagemCoroa;
+        double totalSorteios;
+        int totalCara;
+        int totalCoroa;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSorteio(object sender, RoutedEventArgs e) {
+            totalSorteios++;
+            txtCaraOuCoroa.Visibility = Visibility.Visible;
+            chance = rnd.Next(1, 3);
+            if (chance == 1) {
+                totalCara++;
+                imgCoroa.Visibility = Visibility.Hidden;
+                imgCara.Visibility = Visibility.Visible;
+                txtCaraOuCoroa.Text = "CARA";
+                
+            }
+            else {
+                totalCoroa++;
+                imgCara.Visibility = Visibility.Hidden;
+                imgCoroa.Visibility = Visibility.Visible;
+                txtCaraOuCoroa.Text = "COROA";
+            }
+            porcentagemCara = (totalCara / totalSorteios) * 100;
+            porcentagemCara = Math.Round(porcentagemCara, 2);
+            txtPorcentagemCara.Text = $"{porcentagemCara}";
+            porcentagemCoroa = (totalCoroa / totalSorteios) * 100;
+            porcentagemCoroa = Math.Round(porcentagemCoroa, 2);
+            txtPorcentagemCoroa.Text = $"{porcentagemCoroa}";
+            txtQtdDeSorteios.Text = $"{totalSorteios}";
+        }
+    }
+}
+
